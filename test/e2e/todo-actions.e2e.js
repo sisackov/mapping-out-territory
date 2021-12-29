90b87e95c9fafaea776ba9e4aa67c1a80c2b87a0
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-29 10:47:41 +0200

    todo-actions.e2e.js done

--- test/e2e/todo-actions.e2e.js
+++ test/e2e/todo-actions.e2e.js
@@ -20,7 +20,16 @@ describe('todo actions', () => {
      * 1. Toggling the "completed" button strikes out the todo
      * 2. Toggling it again will undo the strike out
      */
-    throw new Error('test fails because you need to implement it!');
+    // throw new Error('test fails because you need to implement it!');
+
+    cy.get('.new-todo').type('laundry{enter}');
+
+    //visual testings
+    cy.get('.todo-list .toggle').click();
+    cy.get('.todo-list label').should('have.css', 'text-decoration-line', 'line-through');
+
+    cy.get('.todo-list .toggle').click();
+    cy.get('.todo-list label').should('not.have.css', 'text-decoration-line', 'line-through');
   });
 
   it('should clear completed', () => {
@@ -29,6 +38,22 @@ describe('todo actions', () => {
      *
      * 1. The "Clear completed" button in the bottom should clear out all completed todos
      */
-    throw new Error('test fails because you need to implement it!');
+    // throw new Error('test fails because you need to implement it!');
+
+    //create 3 new todos
+    cy.get('.new-todo').type('item-1{enter}');
+    cy.get('.new-todo').type('item-2{enter}');
+    cy.get('.new-todo').type('item-3{enter}');
+
+    //check item-1 and item-3
+    cy.get('.todo-list li:nth-child(1) .toggle').click();
+    cy.get('.todo-list li:nth-child(3) .toggle').click();
+
+    //click clear completed
+    cy.get('.clear-completed').click();
+
+    //check that only item-2 is left
+    cy.get('.todo-list li').should('have.length', 1);
+    cy.get('.todo-list li:nth-child(1) label').should('have.text', 'item-2');
   });
 });
